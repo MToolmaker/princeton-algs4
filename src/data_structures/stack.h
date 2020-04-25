bd14ee72117ca6e660b593d9e41980afd0220152
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-04-25 19:22:14 +0300

    implement stack data structure

---
+++ src/data_structures/stack.h
@@ -0,0 +1,55 @@
+//
+// Created by Ivan Yarkov on 25/04/2020.
+//
+
+#include <memory>
+
+namespace data_structures {
+    template<class T>
+    struct Stack {
+    public:
+        explicit Stack(int capacity) {
+            assert(capacity >= 1);
+            myCapacity = capacity;
+            currentSize = 0;
+            array = new T[myCapacity];
+        }
+
+        void push(T element) {
+            if (currentSize >= myCapacity) {
+                int newSize = 2 * myCapacity;
+                T *newUnderlyingArray = new T[newSize];
+                for (int i = 0; i < myCapacity; i++) {
+                    newUnderlyingArray[i] = array[i];
+                }
+                delete[] array;
+                array = newUnderlyingArray;
+                myCapacity = newSize;
+            }
+            array[currentSize] = element;
+            currentSize++;
+        }
+
+        T pop() {
+            assert(currentSize > 0);
+            return array[--currentSize];
+        }
+
+        T peek() {
+            return array[currentSize - 1];
+        }
+
+        int getSize() {
+            return currentSize;
+        }
+
+        int isEmpty() {
+            return currentSize <= 0;
+        }
+
+    private:
+        int myCapacity;
+        T *array;
+        int currentSize;
+    };
+}
---
+++ tests/data_structures/stack.cpp
@@ -0,0 +1,52 @@
+//
+// Created by Ivan Yarkov on 25/04/2020.
+//
+
+#include <iostream>
+#include "../../src/data_structures/stack.h"
+
+namespace tests {
+    void testStackWithOneElement();
+
+    void testStackResizing();
+
+    void testPeek();
+
+    void runAllStackTests() {
+        testStackWithOneElement();
+        testStackResizing();
+        testPeek();
+        std::cout << "All stack tests passed" << std::endl;
+    }
+
+    void testStackWithOneElement() {
+        data_structures::Stack<char> stack(1);
+        char testLetter = 'a';
+        stack.push(testLetter);
+        assert(stack.getSize() == 1);
+        assert(stack.pop() == testLetter);
+        assert(stack.isEmpty());
+        assert(stack.getSize() == 0);
+    }
+
+    void testStackResizing() {
+        data_structures::Stack<int> stack(1);
+        for (int i = 1; i <= 100; i++) {
+            stack.push(i);
+            assert(stack.getSize() == i);
+        }
+        for (int i = 100; i >= 1; i--) {
+            assert(stack.getSize() == i);
+            assert(stack.pop() == i);
+        }
+    }
+
+    void testPeek() {
+        data_structures::Stack<char> stack(1);
+        char testCharacter = 'a';
+        stack.push(testCharacter);
+        assert(stack.getSize() == 1);
+        assert(stack.peek() == testCharacter);
+        assert(stack.getSize() == 1);
+    }
+}
