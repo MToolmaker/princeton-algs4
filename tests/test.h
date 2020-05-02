e6dc8ba6f8132b5f475f38490e51ffaccfee70ab
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-05-02 11:18:42 +0300

    add include guard for tests runner header

--- tests/test.h
+++ tests/test.h
@@ -2,6 +2,10 @@
 // Created by Ivan Yarkov on 25/04/2020.
 //
 
+#ifndef PRINCETON_ALGS4_TESTS_TEST_H_
+#define PRINCETON_ALGS4_TESTS_TEST_H_
+
 namespace tests {
     void runAllTests();
 }
+#endif //PRINCETON_ALGS4_TESTS_TEST_H_
