615579879ce2f5165b5c0d92f1255317db08258b
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-06-02 21:08:04 +0300

    implement percolation task

---
+++ exercises/percolation/percolation.h
@@ -0,0 +1,11 @@
+//
+// Created by Ivan Yarkov on 27/04/2020.
+//
+
+#ifndef PRINCETON_ALGS4_NEW_PERCOLATION_H
+#define PRINCETON_ALGS4_NEW_PERCOLATION_H
+
+namespace exercises {
+    double findPercolationNumber(int n);
+}
+#endif //PRINCETON_ALGS4_NEW_PERCOLATION_H
