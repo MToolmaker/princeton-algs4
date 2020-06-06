4c1239fa8308a27bb2d271d64befc274b5219104
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-06-06 19:24:36 +0300

    clean up: fix distribution variable name

--- src/exercises/percolation/percolation.cpp
+++ src/exercises/percolation/percolation.cpp
@@ -52,12 +52,12 @@ namespace exercises {
 			unionFind.doUnion(i, n * n + 1);
 		}
 		std::mt19937 generator(current_time_nanoseconds());
-		std::uniform_int_distribution<> six(1, n * n);
+		std::uniform_int_distribution<> distribution(1, n * n);
 		int numberOfOpenSites = 0;
 		int randomElement;
 		do {
 			do {
-				randomElement = six(generator);
+				randomElement = distribution(generator);
 			} while (cellColors[randomElement - 1] == CellColor::WHITE);
 			cellColors[randomElement - 1] = CellColor::WHITE;
 			numberOfOpenSites++;
