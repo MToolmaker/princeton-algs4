cbe4c64d0e4320b86cc07208fb1a4896f63b660f
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-06-06 17:54:23 +0300

    fix include guard name

--- exercises/percolation/percolation.h
+++ exercises/percolation/percolation.h
@@ -2,10 +2,10 @@
 // Created by Ivan Yarkov on 27/04/2020.
 //
 
-#ifndef PRINCETON_ALGS4_NEW_PERCOLATION_H
-#define PRINCETON_ALGS4_NEW_PERCOLATION_H
+#ifndef PRINCETON_ALGS4_PERCOLATION_H
+#define PRINCETON_ALGS4_PERCOLATION_H
 
 namespace exercises {
     double findPercolationNumber(int n);
 }
-#endif //PRINCETON_ALGS4_NEW_PERCOLATION_H
+#endif //PRINCETON_ALGS4_PERCOLATION_H
