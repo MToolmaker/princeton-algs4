f7cfc0d91bb7589f424ce4f8c241e8997df189c7
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-06-07 17:30:24 +0300

    halve underlying array if stack becomes one-quarter full

--- include/data_structures/stack.h
+++ include/data_structures/stack.h
@@ -8,45 +8,48 @@
 #include <memory>
 
 namespace data_structures {
-    template<class T>
-    struct Stack {
-    public:
-        explicit Stack(int capacity) {
-            assert(capacity >= 1);
-            myCapacity = capacity;
+	template<class T>
+	struct Stack {
+	public:
+		explicit Stack(int capacity) {
+			assert(capacity >= 1);
+			myCapacity = capacity;
 			myCurrentSize = 0;
 			myArray = new T[myCapacity];
-        }
+		}
 
-        void push(T element) {
-            if (myCurrentSize >= myCapacity) {
-                resize(2 * myCapacity);
-            }
+		void push(T element) {
+			if (myCurrentSize >= myCapacity) {
+				resize(2 * myCapacity);
+			}
 			myArray[myCurrentSize] = element;
-            myCurrentSize++;
-        }
+			myCurrentSize++;
+		}
 
-        T pop() {
-            assert(myCurrentSize > 0);
-            return myArray[--myCurrentSize];
-        }
+		T pop() {
+			assert(myCurrentSize > 0);
+			if (myCurrentSize == myCapacity / 4) {
+				resize(myCapacity / 2);
+			}
+			return myArray[--myCurrentSize];
+		}
 
-        T peek() {
-            return myArray[myCurrentSize - 1];
-        }
+		T peek() {
+			return myArray[myCurrentSize - 1];
+		}
 
-        int getSize() {
-            return myCurrentSize;
-        }
+		int getSize() {
+			return myCurrentSize;
+		}
 
-        int isEmpty() {
-            return myCurrentSize <= 0;
-        }
+		int isEmpty() {
+			return myCurrentSize <= 0;
+		}
 
-    private:
-        int myCapacity;
-        T *myArray;
-        int myCurrentSize;
+	private:
+		int myCapacity;
+		T *myArray;
+		int myCurrentSize;
 
 		void resize(int newSize) {
 			T *newArray = new T[newSize];
@@ -57,6 +60,6 @@ namespace data_structures {
 			myArray = newArray;
 			myCapacity = newSize;
 		}
-    };
+	};
 }
 #endif //PRINCETON_ALGS4_SRC_DATA_STRUCTURES_STACK_H_
