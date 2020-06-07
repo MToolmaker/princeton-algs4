20f20f706406f20df2f33409fb8d1143a5315db2
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-06-07 17:08:19 +0300

    add output info for running percolation tests successfully

--- tests/src/exercises/percolation_test.cpp
+++ tests/src/exercises/percolation_test.cpp
@@ -6,6 +6,7 @@
 #include <exercises/percolation.h>
 #include <cassert>
 #include <cmath>
+#include <iostream>
 
 static double PERCOLATION_THRESHOLD = 0.592746;
 static double DELTA = 0.01;
@@ -14,5 +15,6 @@ static int GRID_SIDE_SIZE = 1000;
 void tests::runAllPercolationTests() {
 	double percoltaionExperimentThreshold = exercises::findPercolationNumber(GRID_SIDE_SIZE);
 	assert(abs(PERCOLATION_THRESHOLD - percoltaionExperimentThreshold) < DELTA);
+	std::cout << "All percolation exercise tests passed" << std::endl;
 }
 
