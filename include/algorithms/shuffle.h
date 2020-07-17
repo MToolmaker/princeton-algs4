58c16db75f63e9d1d20bee50a15071545134322b
Ivan Yarkov <ivan.yarkov@jetbrains.com>
2020-07-17 22:52:36 +0300

    implement vector shuffle

---
+++ include/algorithms/shuffle.h
@@ -0,0 +1,28 @@
+#include <iso646.h>
+//
+// Created by Ivan Yarkov on 17/07/2020.
+//
+
+#ifndef PRINCETON_ALGS4_INCLUDE_ALGORITHMS_SHUFFLE_H_
+#define PRINCETON_ALGS4_INCLUDE_ALGORITHMS_SHUFFLE_H_
+
+#include <data_structures/vector.h>
+#include <random>
+
+namespace algorithms {
+	template<class T>
+	void swap(data_structures::Vector<T>& vector, int i, int j);
+
+	template<class T>
+	__unused void shuffle(data_structures::Vector<T>& vector) {
+		int size = vector.size();
+		std::random_device dev;
+		std::mt19937 rng(dev());
+		for (int i = 1; i < size; ++i) {
+			std::uniform_int_distribution<std::mt19937::result_type> distribution(0, i);
+			int random = distribution(rng);
+			swap(vector, i, random);
+		}
+	}
+}
+#endif //PRINCETON_ALGS4_INCLUDE_ALGORITHMS_SHUFFLE_H_
